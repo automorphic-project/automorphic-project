c9767b5e8ac693b503da924e1b86fc4f17fdfe5e
automorphic-project <automorphic.project@gmail.com>
2016-11-16 21:22:38 -0500

    Contributors and todo files initialized

--- scripts/tag_up.py
+++ scripts/tag_up.py
@@ -17,7 +17,7 @@ name = argv[2]
 
 def replace_newtheorem(line):
 	if line.find("\\newtheorem{") == 0:
-		line = line.replace("]{", "]{\\href{http://localhost:8080/tag/\\TAG}{",1)
+		line = line.replace("]{", "]{\\href{http://automorphic.newark.rutgers.edu/tag/\\TAG}{",1)
 		line = line.rstrip()
 		return line + "}\n"
 	if line.find("\\documentclass") == 0:
